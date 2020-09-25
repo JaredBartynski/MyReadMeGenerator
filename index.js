600dccdfc0053a13dcc48365a7ec3235715af053
JaredBartynski <70081376+JaredBartynski@users.noreply.github.com>
2020-09-24 20:37:51 -0400

    all input questions finished

--- index.js
+++ index.js
@@ -1,16 +1,62 @@
-// array of questions for user
-const questions = [
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 
-];
+// array of questions for user
+inquirer.prompt([
+  {
+    type: "input",
+    name: "Title",
+    message: "What is the title of the project?",
+  },
+  {
+    type: "input",
+    name: "Description",
+    message: "What is the description of the project?",
+  },
+  {
+    type: "input",
+    name: "Installation",
+    message: "What is the installation proces for the project?",
+  },
+  {
+    type: "input",
+    name: "Usage",
+    message: "What is the usage?",
+  },
+  {
+    type: "checkbox",
+    name: "License",
+    message: "What is the license for the project?",
+    choices: ["MIT", "Apache 2.0", "GPL 3.0", "BSD 3", "None"],
+  },
+  {
+    type: "input",
+    name: "Contributing",
+    message: "Who contributed to the project?",
+  },
+  {
+    type: "input",
+    name: "Tests",
+    message: "What are the tests for the project?",
+  },
+  {
+    type: "input",
+    name: "E-mail",
+    message: "What is your e-mail?",
+  },
+  {
+    type: "input",
+    name: "Github",
+    message: "What is your Github username?",
+  },
+]);
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+function writeToFile(fileName, data) {}
 
 // function to initialize program
-function init() {
-
-}
+function init() {}
 
 // function call to initialize program
 init();
